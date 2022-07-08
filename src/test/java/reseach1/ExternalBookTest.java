95b07ca07308633dc4c763944a8cb91887277239
Juan Moreno <jmoreno@griddynamics.com>
2022-07-08 15:23:11 -0500

    Implement new test to RemoveDuplicates

--- src/test/java/reseach1/ExternalBookTest.java
+++ src/test/java/reseach1/ExternalBookTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -34,20 +35,17 @@ class ExternalBookTest {
     ExternalBook b12 = new ExternalBook( "Pepito Magics",
             "lois Province",1);
 
-    @Test
-    @DisplayName("Book 1, time implement time")
-    @Timeout(10000000)
-    void Book() throws InterruptedException {
+    ExternalBook b13 = new ExternalBook( "Marcel Magics",
+            "lois Province",2);
 
-        List<ExternalBook> list = List.of(b1, b2);
-        List<ExternalBook> list2 = List.of(b2, b4);
+    ExternalBook b14 = new ExternalBook( "Marcel Magics",
+            "lois Province",2);
 
-         ExternalBook.findDuplicates(list, list2);
-        TimeUnit.NANOSECONDS.sleep(10000);
+    ExternalBook b15 = new ExternalBook( "Pepito Magics",
+            "lois Province",1);
 
-
-//        assert Timeout(Duration.of(1), ()-> ExternalBook.findDuplicates(list,list2));
-    }
+    ExternalBook b16 = new ExternalBook( " Magics",
+            "lois Province",2);
 
     @Test
     @DisplayName("Book 2, the size of the new list are 0, when there is not books duplicates")
@@ -59,7 +57,7 @@ class ExternalBookTest {
     }
 
     @Test
-    @DisplayName("Book 3, the size of the new list are 2, when there is books duplicates")
+    @DisplayName("Book 3, the size of the new list are 1, when there is books duplicates")
 
     void Book3(){
         List<ExternalBook> list = List.of(b1, b2,b11);
@@ -69,25 +67,94 @@ class ExternalBookTest {
     }
 
     @Test
-    @DisplayName("Book 4, time measures")
-    void Book4() throws InterruptedException {
-        List<ExternalBook> list = List.of(b1, b2,b11);
-        List<ExternalBook> list2 = List.of(b3, b4, b12);
+    @DisplayName("Book 4, show the time to execution to find duplicates, when the both list has 10 000 000 elements ")
+    void Book4()  {
 
-        Thread.sleep(10000);
+
+        List<ExternalBook> list = new ArrayList<>();
+        List<ExternalBook> list2 = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+
+        for (int x=0; x<10000000; x++)
+        {
+            list.add(b1);
+            list.add(b2);
+            list.add(b11);
+
+            list2.add(b3);
+            list2.add(b4);
+            list.add(b12);
+
+        }
         ExternalBook.findDuplicates(list,list2);
 
-
-
-//        long startTime = System.currentTimeMillis();
-//        ExternalBook.findDuplicates(list,list2);
-//
-//        long estimatedTime = System.currentTimeMillis()-startTime;
-//        assertTrue(estimatedTime<1);
+        long estimatedTime = System.currentTimeMillis()-startTime;
+        System.out.println(estimatedTime);
 
     }
 
 
+    @Test
+    @DisplayName("Book 5, show the time to execution to find duplicates, when the both list has 1000 elements ")
+    void Book5() {
+        List<ExternalBook> list = new ArrayList<>();
+        List<ExternalBook> list2 = new ArrayList<>();
+
+        long startTime = System.currentTimeMillis();
+
+        for (int x=0; x<1000; x++)
+        {
+            list.add(b1);
+            list.add(b2);
+            list.add(b11);
+
+            list2.add(b3);
+            list2.add(b4);
+            list.add(b12);
+
+        }
+        ExternalBook.findDuplicates(list,list2);
+
+        long estimatedTime = System.currentTimeMillis()-startTime;
+        System.out.println(estimatedTime);
+
+    }
+
+    @Test
+    @DisplayName("Book 6, show the time from remove the duplicates, when the collection has 10 000 elements ")
+    void Book6() {
+        List<ExternalBook> list = new ArrayList<>();
 
 
+        long startTime = System.currentTimeMillis();
+
+        for (int x=0; x<10000; x++)
+        {
+            list.add(b1);
+            list.add(b2);
+            list.add(b11);
+            list.add(b3);
+            list.add(b4);
+            list.add(b16);
+            list.add(b13);
+            list.add(b14);
+            list.add(b16);
+        }
+
+        ExternalBook.removeDuplicates(list);
+        long estimatedTime = System.currentTimeMillis()-startTime;
+        System.out.println(estimatedTime);
+
+    }
+
+    @Test
+    @DisplayName("Book 7, the size of the new list are 6, when remove duplicates elements")
+    void Book7() {
+
+        List<ExternalBook> list = List.of(b1,b2,b3,b4,b11,b12,b13,b3,b14,b15,b2,b2,b1,b16);
+       List<ExternalBook> newList = (List<ExternalBook>) ExternalBook.removeDuplicates(list);
+
+        assertEquals(6, newList.size());
+    }
 }
