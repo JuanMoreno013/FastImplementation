c4f284cf96fc8c7c4d1a0a622bf945bffd3d18d5
Juan Moreno <jmoreno@griddynamics.com>
2022-07-08 11:03:14 -0500

    Implement method to find duplicates

--- src/main/java/reseach1/ExternalBook.java
+++ src/main/java/reseach1/ExternalBook.java
@@ -1,9 +1,6 @@
 package reseach1;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.*;
 import java.util.function.Function;
@@ -14,6 +11,7 @@ import java.util.stream.Stream;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public final class ExternalBook {
 
     private String bookName;
@@ -24,7 +22,7 @@ public final class ExternalBook {
 
     private byte[] id;
 
-    private int newId;
+    private static int newId=1;
 
 
     public ExternalBook(String bookName, String bookAuthor ) {
@@ -34,9 +32,15 @@ public final class ExternalBook {
         newId++;
 
 //        this.customData = customData;
-
     }
 
+    public ExternalBook(String bookName, String bookAuthor, int id ) {
+        this.bookName = bookName;
+        this.bookAuthor = bookAuthor;
+        this.id = new byte[id];
+
+//        this.customData = customData;
+    }
 
     public static List<ExternalBook> findDuplicates(Collection<ExternalBook> collOne, Collection<ExternalBook> collTwo) {
     List<ExternalBook> newList = new ArrayList<>();
@@ -48,11 +52,9 @@ public final class ExternalBook {
             for (ExternalBook coll2: collTwo ){
 //                if (coll1.equals(coll2))
                 if (coll1.equals(coll2)) {
-
                     newList.add(coll1);
                     break;
                 }
-
             }
         }
 
@@ -69,24 +71,15 @@ public final class ExternalBook {
 
         ExternalBook book = (ExternalBook) obj;
 
-        return book.getBookName().equals(this.getBookName());
+        boolean conditional = book.getBookName().equals(this.getBookName())
+                && book.getBookAuthor().equals(this.getBookAuthor())
+                && Arrays.equals(book.getId(), this.getId())
+                && Arrays.equals(book.getCustomData(), this.getCustomData());
+
+
+        return conditional;
     }
 
-    public static boolean isEqualCollection(final Collection<?> a, final Collection<?> b) {
-        if(a.size() != b.size()) {
-                        return false;
-                    }
-                final CardinalityHelper<Object> helper = new CardinalityHelper<>(a, b);
-                if(helper.cardinalityA.size() != helper.cardinalityB.size()) {
-                        return false;
-                    }
-                for( final Object obj : helper.cardinalityA.keySet()) {
-                        if(helper.freqA(obj) != helper.freqB(obj)) {
-                                return false;
-                            }
-                    }
-                return true;
-    }
 
 //        collOne.retainAll(collTwo);
 //        List<ExternalBook> list1 = (List<ExternalBook>) collOne;
@@ -113,84 +106,24 @@ public final class ExternalBook {
 
 //    }
 
-    private static class CardinalityHelper<O> {
-        /** Contains the cardinality for each object in collection A. */
-        final  Map<O, Integer> cardinalityA;
-
-        /** Contains the cardinality for each object in collection B. */
-        final Map<O, Integer> cardinalityB;
-
-            public CardinalityHelper(final Iterable<? extends O> a, final Iterable<? extends O> b) {
-                cardinalityA = getCardinalityMap(a);
-                cardinalityB = getCardinalityMap(b);
-        }
-
-//        public static <O> Map<O, Integer> getCardinalityMap(final Iterable<? extends O> coll) {
-//            final Map<O, Integer> count = new HashMap<>();
-//            for (final O obj : coll) {
-//                count.merge(obj, 1, Integer::sum);
-//            }
-//            return count;
-//        }
-
-
-        public final int max(final Object obj) {
-                        return Math.max(freqA(obj), freqB(obj));
-                    }
-
-                public final int min(final Object obj) {
-                        return Math.min(freqA(obj), freqB(obj));
-            }
-
-        public int freqA(final Object obj) {
-                        return getFreq(obj, cardinalityA);
-            }
-
-        public int freqB(final Object obj) {
-                        return getFreq(obj, cardinalityB);
-                    }
-
-        private int getFreq(final Object obj, final Map<?, Integer> freqMap) {
-                        final Integer count = freqMap.get(obj);
-            return Objects.requireNonNullElse(count, 0);
-        }
-    }
-
-
-
-    public static <O> Map<O, Integer> getCardinalityMap(final Iterable<? extends O> coll) {
-                final Map<O, Integer> count = new HashMap<>();
-                for (final O obj : coll) {
-                        final Integer c = count.get(obj);
-                        if (c == null) {
-                                count.put(obj, Integer.valueOf(1));
-                            } else {
-                                count.put(obj, Integer.valueOf(c.intValue() + 1));
-                            }
-                    }
-                return count;
-            }
 
     public static <T> Collection<T> removeDuplicates(Collection<T> collection) {
         return null;
     }
 
-    public static boolean compareList(Collection<ExternalBook> collOne, Collection<ExternalBook> collTwo)
-    {
-            return false;
-    }
+
 
     public static void main(String[] args) {
 
-        ExternalBook b1 = new ExternalBook( "Ppepeito Magis",
+        ExternalBook b1 = new ExternalBook( "Pepito Magics",
                 "lois Province");
 
-        ExternalBook b2 = new ExternalBook( "Ppepeito Magi",
+        ExternalBook b2 = new ExternalBook( "Marcel Magics",
                 "lois Province");
 
-        ExternalBook b3 = new ExternalBook( "Ppepeito Magis",
+        ExternalBook b3 = new ExternalBook( "Marcel Magics",
                 "lois Province");
-        ExternalBook b4 = new ExternalBook( "Ppepeito Magis",
+        ExternalBook b4 = new ExternalBook( "Pepito Magics",
                 "lois Province");
 
 //        ExternalBook b2 = new ExternalBook( "Ppepeito Magis",
@@ -208,8 +141,9 @@ public final class ExternalBook {
         Collection<ExternalBook> list2 = List.of(b3, b4);
 
 
-        System.out.println(findDuplicates(list,list2));
+        Collection<ExternalBook> finalList = findDuplicates(list,list2);
 
-//        System.out.println(isEqualCollection(list, list2));
+        System.out.println(finalList.toString());
+
     }
 }
