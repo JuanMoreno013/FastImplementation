18f4806d95c3041137724abc8e07d0e7732d6d44
Juan Moreno <jmoreno@griddynamics.com>
2022-08-01 10:31:04 -0500

    Implement Arrays.equals

--- src/main/java/research2/ByteArray.java
+++ src/main/java/research2/ByteArray.java
@@ -15,21 +15,14 @@ public class ByteArray {
         this.data2 = data2;
     }
 
-    public static <T> boolean compareElements(T[] data, T[] data2) {
+    public static boolean compareElements(final byte[] data, final byte[] data2) {
         if (data2.length != data.length)
             return false;
 
-        List<T> byteSet = List.of(data2);
-//        Set<T> byteSet = new HashSet<>(Arrays.asList(data2));
+        List<byte[]> hasList = List.of(data2);
 
-        for (T byteCompare : data) {
-            if (byteSet.equals(byteCompare)) {
-                return true;
-            }
-        }
-        return false;
+        return hasList.stream().allMatch(i -> Arrays.equals(data, data2));
     }
-
 
     public static boolean equals(byte[] data1, byte[] data2) {
         if (data1 == data2)
